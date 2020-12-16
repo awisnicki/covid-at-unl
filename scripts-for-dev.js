2204476ed065d1f529055cdc651e9b0046b06979
Adrian Wisnicki <awisnicki@yahoo.com>
2020-12-15 20:32:32 -0600

    create scripts dev

---
+++ scripts-for-dev.js
@@ -0,0 +1,21 @@
+/* Service Worker */
+/* Taken from https://pwa-workshop.js.org/2-service-worker/#registering-the-service-worker */
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then(serviceWorker => {
+        console.log("Service Worker registered: ", serviceWorker);
+      })
+      .catch(error => {
+        console.error("Error registering the Service Worker: ", error);
+      });
+  }
+  
+  AddRelNoopener();
+  
+  function AddRelNoopener(){
+    var links = document.querySelectorAll("a");
+    for(var i = 0; i < links.length; i++){
+      links[i].setAttribute("rel","noopener");
+    }
+  }	
